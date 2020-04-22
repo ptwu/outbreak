5587dcf25ed0656572ff85cf524103d2aa596c50
root <root@DESKTOP-514J5BH.localdomain>
2020-04-21 23:32:50 -0400

    virus changes

--- src/virus.ml
+++ src/virus.ml
@@ -36,10 +36,24 @@ let getdrug v =
 
 let upgrade upg_type upg_val v =
   match upg_type with
-  | "infectivity" -> {infectivity = (getinfect v)+(upg_val); severity = (getsever v); lethality = (getlethal v); heat_res = (getheat v); cold_res = (getcold v); drug_res = (getdrug v)}
-  | "severity" -> {infectivity = (getinfect v); severity = (getsever v)+(upg_val); lethality = (getlethal v); heat_res = (getheat v); cold_res = (getcold v); drug_res = (getdrug v)}
-  | "lethality" -> {infectivity = (getinfect v); severity = (getsever v); lethality = (getlethal v)+(upg_val); heat_res = (getheat v); cold_res = (getcold v); drug_res = (getdrug v)}
-  | "heat_res" -> {infectivity = (getinfect v); severity = (getsever v); lethality = (getlethal v); heat_res = (getheat v)+(upg_val); cold_res = (getcold v); drug_res = (getdrug v)}
-  | "cold_res" -> {infectivity = (getinfect v); severity = (getsever v); lethality = (getlethal v); heat_res = (getheat v); cold_res = (getcold v)+(upg_val); drug_res = (getdrug v)}
-  | "drug_res" -> {infectivity = (getinfect v); severity = (getsever v); lethality = (getlethal v); heat_res = (getheat v); cold_res = (getcold v); drug_res = (getdrug v)+(upg_val)}
+  | "infectivity" -> {infectivity = (getinfect v)+(upg_val);
+                      severity = (getsever v); lethality = (getlethal v);
+                      heat_res = (getheat v); cold_res = (getcold v);
+                      drug_res = (getdrug v)}
+  | "severity" -> {infectivity = (getinfect v); 
+                   severity = (getsever v)+(upg_val); lethality = (getlethal v);
+                   heat_res = (getheat v); cold_res = (getcold v);
+                   drug_res = (getdrug v)}
+  | "lethality" -> {infectivity = (getinfect v); severity = (getsever v);
+                    lethality = (getlethal v)+(upg_val); heat_res = (getheat v);
+                    cold_res = (getcold v); drug_res = (getdrug v)}
+  | "heat_res" -> {infectivity = (getinfect v); severity = (getsever v);
+                   lethality = (getlethal v); heat_res = (getheat v)+(upg_val);
+                   cold_res = (getcold v); drug_res = (getdrug v)}
+  | "cold_res" -> {infectivity = (getinfect v); severity = (getsever v);
+                   lethality = (getlethal v); heat_res = (getheat v);
+                   cold_res = (getcold v)+(upg_val); drug_res = (getdrug v)}
+  | "drug_res" -> {infectivity = (getinfect v); severity = (getsever v);
+                   lethality = (getlethal v); heat_res = (getheat v);
+                   cold_res = (getcold v); drug_res = (getdrug v)+(upg_val)}
   | _ -> failwith "Invalid Upgrade"
