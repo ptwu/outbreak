8a2155b7b345dd0e2df7677aa0f7681243b02f5b
Peter Wu <peterlingwu@gmail.com>
2020-04-24 22:03:10 -0400

    add world.ml and corresponding interface

--- src/virus.mli
+++ src/virus.mli
@@ -1,26 +1,17 @@
-(** [t] is a virus *)
 type t
 
-(** [init_virus] is the state of a virus at the start of the game *)
 val init_virus : t
 
-(** [empty_upgrade] is a virus upgrade with empty stats *)
 val empty_upgrade : t
 
-(** [get_infect] is a function that returns the infectivity of a virus [t] *)
 val get_infect : t -> int
 
-(** [get_sever] is a function that returns the severity of a virus [t] *)
 val get_sever : t -> int
 
-(** [get_lethal] is a function that returns the lethality of a virus [t] *)
 val get_lethal : t -> int
 
-(** [get_heat] is a function that returns the heat resistance of a virus [t] *)
 val get_heat : t -> int
 
-(** [get_cold] is a function that returns the cold resistance of a virus [t] *)
 val get_cold : t -> int
 
-(** [get_drug] is a function that returns the drug resistance of a virus [t] *)
 val upgrade : t -> t -> t
