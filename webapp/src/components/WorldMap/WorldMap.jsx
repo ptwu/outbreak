ee86ac269ed122ec38276cceb82af4446564c16a
Peter Wu <peterlingwu@gmail.com>
2020-05-04 19:26:39 -0400

    Add favicons and recurring useEffect fetch to WorldMap

--- webapp/src/components/WorldMap/WorldMap.jsx
+++ webapp/src/components/WorldMap/WorldMap.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './WorldMap.module.css';
+import {
+  Card, Container,
+} from '@material-ui/core';
 
-export default () => (
-  <>
-    <p>hewo i am the world map</p>
-  </>
-);
+export default ({ initHealthy }) => {
+  const [healthy, setHealthy] = useState(initHealthy);
+  const [infected, setInfected] = useState(0);
+  const [deaths, setDeaths] = useState(0);
+  const [cureProgress, setCureProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await fetch('/step')
+        .then(res => res.json())
+        .then
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <Card className={styles.MainMenuCard}>
+          
+        </Card>
+      </Container>
+    </>
+  )
+}
