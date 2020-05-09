0bf919fb2bfde6cef605dfccb3b3d4b04e65bd54
Enoch Chen <sirenochen@gmail.com>
2020-05-09 19:41:14 -0400

    Everything should be documented now

--- src/virus.mli
+++ src/virus.mli
@@ -1,5 +1,4 @@
 (** Representation of virus game model. *)
-
 open Upgrades
 open Stats
 
@@ -17,16 +16,22 @@ val init_virus : t
 (** [infectivity v] is the infectivity stat of the virus [v] *)
 val infectivity : t -> float
 
+(** [severity v] is is the severity stat of [v]. *)
 val severity : t -> float
 
+(** [hality v] is is the hality stat of [v]. *)
 val hality : t -> float
 
+(** [heat_res v] is is the heat_res stat of [v]. *)
 val heat_res : t -> float
 
+(** [cold_res v] is is the cold_res stat of [v]. *)
 val cold_res : t -> float
 
+(** [drug_res v] is is the drug_res stat of [v]. *)
 val drug_res : t -> float
 
+(** [anti_cure v] is is the anti_cure stat of [v]. *)
 val anti_cure : t -> float
 
 (** [add_points p v] is [v] with [p] additional points. *)
