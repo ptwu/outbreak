eb7a16cf697cd7ef43b4ca10c2ae34828d9da53b
Enoch Chen <sirenochen@gmail.com>
2020-05-10 19:20:34 -0400

    update instructions

--- webapp/src/components/Menu/Menu.jsx
+++ webapp/src/components/Menu/Menu.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useRef } from 'react';
-import styles from './Menu.module.css';
-import {
-  Card, Container, Grid, Button
-} from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React, { useState, useRef } from "react";
+import styles from "./Menu.module.css";
+import { Card, Container, Grid, Button } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default ({ menuOptionHandler }) => {
   const [isInInstructions, setInstructionsState] = useState(false);
   const [open, setOpen] = React.useState(false);
-  const nameTextRef = useRef('');
+  const nameTextRef = useRef("");
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
-    if (nameTextRef.current.value !== '') {
+    if (nameTextRef.current.value !== "") {
       setOpen(false);
       menuOptionHandler(false, nameTextRef.current.value);
     }
@@ -28,7 +26,13 @@ export default ({ menuOptionHandler }) => {
   return (
     <>
       <div>
-        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" className={styles.DialogWrap} disableBackdropClick>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+          className={styles.DialogWrap}
+          disableBackdropClick
+        >
           <DialogTitle id="form-dialog-title">Name Your Outbreak</DialogTitle>
           <DialogContent>
             <TextField
@@ -44,12 +48,8 @@ export default ({ menuOptionHandler }) => {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={() => setOpen(false)}>
-              Cancel
-            </Button>
-            <Button onClick={handleClose}>
-              Start Game
-            </Button>
+            <Button onClick={() => setOpen(false)}>Cancel</Button>
+            <Button onClick={handleClose}>Start Game</Button>
           </DialogActions>
         </Dialog>
       </div>
@@ -58,64 +58,61 @@ export default ({ menuOptionHandler }) => {
           <Container maxWidth="sm">
             <h1>Outbreak</h1>
             <p>
-              A CS 3110 Final Project by Enoch Chen, Peter Wu, and Shefali Janorkar.
-          </p>
-            {isInInstructions
-              ? (<div className={styles.InstructionsSection}>
-                <h1>Instructions</h1>
-                <p>You start by choosing a disease to play and picking a country
-                to start in. Depending on the attributes of your disease, it
-                infects people in that country at a certain rate with certain
-                symptoms. The simulation model will take into account the status
-                of different countries (for example, the UK shutting down airports
-                would make it less likely for your disease to enter) and determine
-                how your disease spreads. Different “genes” which can improve
-                infection rate, create fatal symptoms, or improve cure resistance
-                can be bought as you infect people and earn “points”. As you
-                progress and the disease and its symptoms become recognized
-                across a sizable amount of people, borders shut down and the
-                world starts to counter your disease with a cure. Your goal is
-                to infect and kill the whole world before the cure is created and
-                distributed.
+              A CS 3110 Final Project by Enoch Chen, Peter Wu, and Shefali
+              Janorkar.
             </p>
+            {isInInstructions ? (
+              <div className={styles.InstructionsSection}>
+                <h1>Instructions</h1>
+                <p>
+                  You start by choosing a disease to play and picking a region
+                  to start in. Depending on the attributes of your disease, it
+                  infects (and kills) people in that region at a certain rate
+                  with certain symptoms. Earn 🧬 DNA points over time and use
+                  them to purchase gene upgrades. Different genes can improve
+                  infection rate and/or create fatal symptoms. As you progress
+                  and the disease and its symptoms become recognized across a
+                  sizable amount of people, the world starts to counter your
+                  disease with a cure. Your goal is to infect and kill the whole
+                  world before the cure is created and distributed.
+                </p>
                 <Button
                   variant="contained"
                   className={styles.MainMenuButton}
                   onClick={handleClickOpen}
                 >
                   Start
-              </Button>
+                </Button>
               </div>
-              ) : (
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <Grid container justify="center" spacing={8}>
-                      <Grid item>
-                        <Button
-                          variant="contained"
-                          className={styles.MainMenuButton}
-                          onClick={handleClickOpen}
-                        >
-                          Play
-                    </Button>
-                      </Grid>
-                      <Grid item>
-                        <Button
-                          variant="contained"
-                          className={styles.MainMenuButton}
-                          onClick={() => setInstructionsState(true)}
-                        >
-                          Instructions
-                    </Button>
-                      </Grid>
+            ) : (
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Grid container justify="center" spacing={8}>
+                    <Grid item>
+                      <Button
+                        variant="contained"
+                        className={styles.MainMenuButton}
+                        onClick={handleClickOpen}
+                      >
+                        Play
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button
+                        variant="contained"
+                        className={styles.MainMenuButton}
+                        onClick={() => setInstructionsState(true)}
+                      >
+                        Instructions
+                      </Button>
                     </Grid>
                   </Grid>
                 </Grid>
-              )
-            }
+              </Grid>
+            )}
           </Container>
         </Card>
       </Container>
     </>
   );
-}
+};
