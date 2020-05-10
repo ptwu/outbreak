40789064fa93bc4f56200db15149ca61dc0d2e5d
Peter Wu <peterlingwu@gmail.com>
2020-05-10 16:13:29 -0400

    Add offline server detection in frontend

--- webapp/src/components/GameContainer/GameContainer.jsx
+++ webapp/src/components/GameContainer/GameContainer.jsx
@@ -10,11 +10,17 @@ import {
   DialogActions,
   Grid,
   CardContent,
-  Typography
+  Typography,
+  Snackbar,
 } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import WorldMap from './WorldMap';
 import ReactTooltip from 'react-tooltip';
 import InitGameDataJSON from './data/sample_game.json';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default ({ virusName }) => {
   const [name, setName] = useState(virusName);
@@ -29,8 +35,9 @@ export default ({ virusName }) => {
   const [deaths, setDeaths] = useState(0);
   const [shop, setShop] = useState([]);
   const [startTime, setStartTime] = useState(0);
-  const [open, setOpen] = React.useState(false);
-
+  const [open, setOpen] = useState(false);
+  const [toastOpen, setToastOpen] = useState(false);
+  const [isErrorPresent, setError] = useState(false);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -39,31 +46,42 @@ export default ({ virusName }) => {
     setOpen(false);
   };
 
-  const handlePurchase = async (itemId) => {
-    await fetch(`/purchase/${itemId}`, {
-      method: 'POST',
-    });
+  const handlePurchase = async (itemId, itemCost) => {
+    if (itemCost > points) {
+      setToastOpen(true);
+    } else {
+      setToastOpen(true);
+      await fetch(`/purchase/${itemId}`, {
+        method: 'POST',
+      })
+        .then(() => { }, (err) => console.log(err));
+      console.log(itemId + ' ' + itemCost);
+    }
   }
 
   const pickStartingCountryHandler = async (countryName) => {
     setStartingCountry(countryName);
-    await fetch('/reset', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(InitGameDataJSON),
-    })
-      .then(() => { }, (err) => console.log(err));
-    await fetch('/init', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({ name: virusName, starter: countryName }),
-    })
-      .then((data) => data.json(), (err) => console.log(err))
-      .then(d => gameStateHandler(d));
+    try {
+      await fetch('/reset', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(InitGameDataJSON),
+      })
+        .then(() => { }, (err) => console.log(err));
+      await fetch('/init', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({ name: virusName, starter: countryName }),
+      })
+        .then((data) => data.json(), (err) => console.log(err))
+        .then(d => gameStateHandler(d));
+    } catch (error) {
+      setError(true);
+    }
     setStartTime(performance.now());
   }
 
@@ -97,7 +115,7 @@ export default ({ virusName }) => {
     if (startingCountry !== '') {
       const interval = setInterval(async () => {
         await fetch('/step', { method: 'POST' })
-          .then((data) => data.json(), (err) => console.log(err))
+          .then((data) => data.json(), (err) => { setError(true); })
           .then(d => { gameStateHandler(d); });
       }, 1000);
       return () => clearInterval(interval);
@@ -106,96 +124,108 @@ export default ({ virusName }) => {
 
   const [tooltipContent, setTooltipContent] = useState('');
 
-  if (score === 0) {
-    return (
-      <>
-        <div>
-          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" disableBackdropClick>
-            <DialogTitle id="form-dialog-title">Shop</DialogTitle>
-            <DialogContent>
-              <Grid container>
-                <Grid item xs={12}>
-                  <Grid container justify="center" spacing={8}>
-                    {
-                      shop.map((item, i) => (
-                        <Grid item xs key={i}>
-                          <Card>
-                            <CardContent>
-                              <Typography gutterBottom variant="h5" component="h2">
-                                {item.name}
-                              </Typography>
-                              <Typography variant="body2" color="textSecondary" component="p" align="left">
-                                Cost: {item.cost}
-                              </Typography>
-                            </CardContent>
-                            <Button size="large" onClick={() => handlePurchase(item.id)}>
-                              Buy
+  if (!isErrorPresent) {
+    if (score === 0) {
+      return (
+        <>
+
+          <div>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" disableBackdropClick>
+              <DialogTitle id="form-dialog-title">Shop</DialogTitle>
+              <DialogContent>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <Grid container justify="center" spacing={8}>
+                      {
+                        shop.map((item, i) => (
+                          <Grid item xs key={i}>
+                            <Card>
+                              <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                  {item.name}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p" align="left">
+                                  Cost: {item.cost}
+                                </Typography>
+                              </CardContent>
+                              <Button size="large" onClick={() => handlePurchase(item.id, item.cost)}>
+                                Buy
                             </Button>
-                          </Card>
-                        </Grid>
-                      ))
-                    }
+                            </Card>
+                          </Grid>
+                        ))
+                      }
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>
-                Exit
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>
+                  Exit
               </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-        <Container maxWidth="xl">
-          <Card className={styles.GameplayCard} style={{ position: 'relative' }}>
-            <div className={styles.WorldMapContainer}>
-              <h1 className={styles.VirusNameText}>{name}</h1>
-              {startingCountry === ''
-                ? <h2>Choose a continent to start your outbreak!</h2>
-                : <h2>Your Outbreak started in {startingCountry}</h2>}
-              <WorldMap setContent={setTooltipContent} pickCountryHandler={pickStartingCountryHandler} data={countryData} />
-              <ReactTooltip>{tooltipContent}</ReactTooltip>
-            </div>
+              </DialogActions>
+            </Dialog>
+          </div>
+          <Container maxWidth="xl">
+            <Card className={styles.GameplayCard} style={{ position: 'relative' }}>
+              <div className={styles.WorldMapContainer}>
+                <h1 className={styles.VirusNameText}>{name}</h1>
+                {startingCountry === ''
+                  ? <h2>Choose a continent to start your outbreak!</h2>
+                  : <h2>Your Outbreak started in {startingCountry}</h2>}
+                <WorldMap setContent={setTooltipContent} pickCountryHandler={pickStartingCountryHandler} data={countryData} />
+                <ReactTooltip>{tooltipContent}</ReactTooltip>
+              </div>
 
-            {startingCountry !== ''
-              ? <>
-                <div className={styles.DateDisplay}>
-                  <p>📅 <b>Date</b>: {getDate()} </p>
-                </div>
-                <div className={styles.VirusStats}>
-                  <p>💓 <b>Healthy</b>: {healthy}
+              {startingCountry !== ''
+                ? <>
+                  <div className={styles.DateDisplay}>
+                    <p>📅 <b>Date</b>: {getDate()} </p>
+                  </div>
+                  <div className={styles.VirusStats}>
+                    <p>💓 <b>Healthy</b>: {healthy}
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 ☣️ <b>Infected</b>: {infected}
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 💀 <b>Dead</b>: {deaths}</p>
-                </div>
-                <div className={styles.CureProgress}>
-                  <p>🧪 <b>Cure Progress</b>: {Math.round(cureProgress)}%</p>
-                </div>
-                <div className={styles.ShopLHS}>
-                  <p>🧬 <b>DNA Points</b>: {points}</p>
+                  </div>
+                  <div className={styles.CureProgress}>
+                    <p>🧪 <b>Cure Progress</b>: {Math.round(cureProgress)}%</p>
+                  </div>
+                  <div className={styles.ShopLHS}>
+                    <p>🧬 <b>DNA Points</b>: {points}</p>
 
-                  <Button variant="contained" onClick={handleClickOpen} className={styles.ShopButton}>
-                    Shop
+                    <Button variant="contained" onClick={handleClickOpen} className={styles.ShopButton}>
+                      Shop
                   </Button>
-                </div>
-              </>
-              : undefined}
+                  </div>
+                </>
+                : undefined}
 
+            </Card>
+          </Container>
+        </>
+      );
+    } else {
+      return (
+        <Container maxWidth="lg">
+          <Card className={styles.GameplayCard}>
+            {cureProgress >= 100 ? <h1 style={{ color: '#A60000' }}>You Lose</h1> : <h1 style={{ color: '#008a25' }}>You Win</h1>}
+            {cureProgress >= 100
+              ? <h3>A vaccine has been discovered for {name}, and the world is back to <i>functional</i> order.</h3>
+              : <h3>{name} has wrought havoc on the entire world!</h3>}
+            <br />
+            <h1>Final Score: {points}</h1>
           </Card>
         </Container>
-      </>
-    );
+      );
+    }
   } else {
     return (
       <Container maxWidth="lg">
         <Card className={styles.GameplayCard}>
-          {cureProgress >= 100 ? <h1 style={{ color: '#A60000' }}>You Lose</h1> : <h1 style={{ color: '#008a25' }}>You Win</h1>}
-          {cureProgress >= 100
-            ? <h3>A vaccine has been discovered for {name}, and the world is back to <i>functional</i> order.</h3>
-            : <h3>{name} has wrought havoc on the entire world!</h3>}
-          <br />
-          <h1>Final Score: {points}</h1>
+          <h1>Error: Server is likely not running.</h1>
+          <h3>Have you run `make server` in the root folder?</h3>
         </Card>
       </Container>
     );
